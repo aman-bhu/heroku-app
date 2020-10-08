7f84b72b9344bad86c9fca536b0c6794b08d2ba0
davidcode00 <46863354+davidcode00@users.noreply.github.com>
2020-10-09 02:04:14 +0530

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -5,7 +5,40 @@ using namespace std;
 signed main(){
     int t = 1; cin >> t;
     while(t--){
-        int a,b,c; cin >> a >> b >> c;
-        cout << (a+b+c-1)  << endl;
+        int n,m; cin >> n >> m;
+        int a[n][m];
+        for(int i = 0; i < n; i++)
+            for(int j = 0; j < m; j++)
+                cin >> a[i][j];
+        int ans = 0;
+        for(int i = 0; i < (n+1)/2; i++)
+            for(int j = 0; j < (m+1)/2; j++)
+            {
+                map<pair<int,int>,bool> mp;
+                vector<int> num;
+                mp[{i,j}] = 1;
+                num.push_back(a[i][j]);
+                if(mp[{n-1-i,j}] == 0)
+                {
+                    mp[{n-1-i,j}] = 1;
+                    num.push_back(a[n-1-i][j]);
+                }
+                if(mp[{i,m-1-j}] == 0)
+                {
+                    mp[{i,m-1-j}] = 1;
+                    num.push_back(a[i][m-1-j]);
+                }
+                if(mp[{n-1-i,m-1-j}] == 0)
+                {
+                    mp[{n-1-i,m-1-j}] = 1;
+                    num.push_back(a[n-1-i][m-1-j]);
+                }
+                sort(num.begin(),num.end());
+                int idx = num.size() - 1;
+                idx /= 2;
+                for(auto x:num)
+                    ans += abs(x - num[idx]);
+            }
+        cout << ans << endl;
     }    
 }
